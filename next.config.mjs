1b20b22cba86a7015880623c2b27cebcfad9d7d6
Carlos Acosta <carlos.acosta@holagpt.es>
2026-04-22 18:52:48 -0400

    Fix Supabase project URL in next.config remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ttbdzjrzifansqbzxlux.supabase.co",
+        hostname: "vfftynfyxazjlzfrhwiw.supabase.co",
       },
     ],
   },
